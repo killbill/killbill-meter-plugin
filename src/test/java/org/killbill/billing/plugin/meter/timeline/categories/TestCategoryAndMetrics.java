b56139b09726c1bcd51e54f12f6e231bc544d396
trifonnt <trifont@gmail.com>
2015-06-29 15:05:33 +0300

    Fixed JUnit test TestCategoryAndMetrics, test now compares both possible cases of JSON response.

--- src/test/java/org/killbill/billing/plugin/meter/timeline/categories/TestCategoryAndMetrics.java
+++ src/test/java/org/killbill/billing/plugin/meter/timeline/categories/TestCategoryAndMetrics.java
@@ -25,43 +25,45 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class TestCategoryAndMetrics extends MeterTestSuiteNoDB {
 
-    @Test(groups = "fast")
-    public void testMapping() throws Exception {
-        final CategoryAndMetrics kinds = new CategoryAndMetrics("JVM");
-        kinds.addMetric("GC");
-        kinds.addMetric("CPU");
+	@Test(groups = "fast")
+	public void testMapping() throws Exception {
+		final CategoryAndMetrics kinds = new CategoryAndMetrics("JVM");
+		kinds.addMetric("GC");
+		kinds.addMetric("CPU");
 
-        final ObjectMapper mapper = new ObjectMapper();
-        final String json = mapper.writeValueAsString(kinds);
-        Assert.assertEquals("{\"eventCategory\":\"JVM\",\"metrics\":[\"GC\",\"CPU\"]}", json);
+		final ObjectMapper mapper = new ObjectMapper();
+		final String json = mapper.writeValueAsString(kinds);
+		// Order of "GC" and "CPU" depends on JVM implementation, that's why test of one of the cases is valid!
+		Assert.assertTrue("{\"eventCategory\":\"JVM\",\"metrics\":[\"GC\",\"CPU\"]}".equals(json)
+					   || "{\"eventCategory\":\"JVM\",\"metrics\":[\"CPU\",\"GC\"]}".equals(json));
 
-        final CategoryAndMetrics kindsFromJson = mapper.readValue(json, CategoryAndMetrics.class);
-        Assert.assertEquals(kindsFromJson, kinds);
-    }
+		final CategoryAndMetrics kindsFromJson = mapper.readValue(json, CategoryAndMetrics.class);
+		Assert.assertEquals(kindsFromJson, kinds);
+	}
 
-    @Test(groups = "fast")
-    public void testComparison() throws Exception {
-        final CategoryAndMetrics aKinds = new CategoryAndMetrics("JVM");
-        aKinds.addMetric("GC");
-        aKinds.addMetric("CPU");
-        Assert.assertEquals(aKinds.compareTo(aKinds), 0);
+	@Test(groups = "fast")
+	public void testComparison() throws Exception {
+		final CategoryAndMetrics aKinds = new CategoryAndMetrics("JVM");
+		aKinds.addMetric("GC");
+		aKinds.addMetric("CPU");
+		Assert.assertEquals(aKinds.compareTo(aKinds), 0);
 
-        final CategoryAndMetrics bKinds = new CategoryAndMetrics("JVM");
-        bKinds.addMetric("GC");
-        bKinds.addMetric("CPU");
-        Assert.assertEquals(aKinds.compareTo(bKinds), 0);
-        Assert.assertEquals(bKinds.compareTo(aKinds), 0);
+		final CategoryAndMetrics bKinds = new CategoryAndMetrics("JVM");
+		bKinds.addMetric("GC");
+		bKinds.addMetric("CPU");
+		Assert.assertEquals(aKinds.compareTo(bKinds), 0);
+		Assert.assertEquals(bKinds.compareTo(aKinds), 0);
 
-        final CategoryAndMetrics cKinds = new CategoryAndMetrics("JVM");
-        cKinds.addMetric("GC");
-        cKinds.addMetric("CPU");
-        cKinds.addMetric("Something else");
-        Assert.assertTrue(aKinds.compareTo(cKinds) < 0);
-        Assert.assertTrue(cKinds.compareTo(aKinds) > 0);
+		final CategoryAndMetrics cKinds = new CategoryAndMetrics("JVM");
+		cKinds.addMetric("GC");
+		cKinds.addMetric("CPU");
+		cKinds.addMetric("Something else");
+		Assert.assertTrue(aKinds.compareTo(cKinds) < 0);
+		Assert.assertTrue(cKinds.compareTo(aKinds) > 0);
 
-        final CategoryAndMetrics dKinds = new CategoryAndMetrics("ZVM");
-        dKinds.addMetric("GC");
-        Assert.assertTrue(aKinds.compareTo(dKinds) < 0);
-        Assert.assertTrue(dKinds.compareTo(aKinds) > 0);
-    }
+		final CategoryAndMetrics dKinds = new CategoryAndMetrics("ZVM");
+		dKinds.addMetric("GC");
+		Assert.assertTrue(aKinds.compareTo(dKinds) < 0);
+		Assert.assertTrue(dKinds.compareTo(aKinds) > 0);
+	}
 }
